ce2e91366f7d9b32b451129d86b1035b69d4fb62
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2017-09-13 20:24:29 +0300

    initial commit

---
+++ cloud_functions/list_events/index.js
@@ -0,0 +1,33 @@
+'use strict';
+const ical = require('ical');
+const moment = require('moment');
+
+const EVENTS_URL = 'http://tietokilta.fi/kalenteri/ical';
+
+/*
+* HTTP Cloud Function.
+*
+* @param {Object} req Cloud Function request context.
+* @param {Object} res Cloud Function response context.
+*/
+exports.helloHttp = function helloHttp (req, res) {
+
+    const eventsData = ical.parseFile('events.ics');
+    const events = [];
+
+    for (var k in eventsData){
+        if (eventsData.hasOwnProperty(k)) {
+            const evnt = eventsData[k];
+            if(evnt.type === "VEVENT" && moment(evnt.end).isAfter()) {
+                events.push(evnt);
+            }
+        }
+    }
+
+    const response = 'There are ' + events.length + ' upcoming events. ' +
+        'Next one is ' + events[0].summary + ' on ' + moment(events[0].start).format('dddd, MMMM Do YYYY, h:mm a');
+
+    res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+    res.send(JSON.stringify({ "speech": response, "displayText": response}));
+
+};
